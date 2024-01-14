121bd7a8c9990a692f0c4c187cfbb58e6c8cc8de
zahoor-uzair <uzaorzahoor.pixako@gmail.com>
2024-01-14 19:06:26 +0500

    some minor improvements

--- src/components/sections/associatesSection.tsx
+++ src/components/sections/associatesSection.tsx
@@ -1,6 +1,19 @@
 import React from "react";
 import ImageCard from "../home/lists/imageCard";
 const AssociatesSection = () => {
+  const imageSources = [
+    "https://travelfly.ai/wp-content/uploads/2023/12/ho-1-300x131.png",
+    "https://travelfly.ai/wp-content/uploads/2023/12/1200px-Aviasales_logo_horizontal-1-300x69-1.png",
+    "https://travelfly.ai/wp-content/uploads/2023/12/economy-bookings-logo-vector-1-300x167-1.png",
+    "https://travelfly.ai/wp-content/uploads/2023/12/kiwitaxi-1-1.png",
+    "https://travelfly.ai/wp-content/uploads/2023/12/download.png",
+    "https://travelfly.ai/wp-content/uploads/2023/12/images-300x131.png",
+    "https://travelfly.ai/wp-content/uploads/2023/12/1571736263_bikesbooking_mid.png",
+    "https://travelfly.ai/wp-content/uploads/2023/12/Trip-logo-h-default-lg-removebg-preview.png",
+    "https://travelfly.ai/wp-content/uploads/2023/12/0x0-1024x239-1.png",
+    "https://travelfly.ai/wp-content/uploads/2023/12/Amazon-logo-1024x430-1.png",
+  ];
+
   return (
     <>
       {" "}
@@ -14,16 +27,9 @@ const AssociatesSection = () => {
           </h2>
         </div>
         <div className=" flex flex-wrap justify-between align-middle">
-          <ImageCard />
-          <ImageCard />
-          <ImageCard />
-          <ImageCard />
-          <ImageCard />
-          <ImageCard />
-          <ImageCard />
-          <ImageCard />
-          <ImageCard />
-          <ImageCard />
+          {imageSources.map((src, index) => (
+            <ImageCard key={index} src={src} />
+          ))}
         </div>
       </div>
     </>
